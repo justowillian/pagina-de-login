766e00ed68d356409570e88c93a51ffc30af6fad
justowillian <justowillian12@gmail.com>
2025-06-03 09:31:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const EMAIL_CORRETO = "Jw12@gmail.com";
+const SENHA_CORRETA = "'jw12345'";
+
+function fazerLogin() {
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+    const mensagem = document.getElementById("mensagem");
+
+    if (email === EMAIL_CORRETO && senha === SENHA_CORRETA) {
+        mensagem.style.color = "green";
+        mensagem.textContent = "Login realizado com sucesso!";
+        setTimeout(() => {
+            window.location.href = "login.html"; 
+        }, 1000);
+    } else {
+        mensagem.style.color = "red";
+        mensagem.textContent = "Email ou senha incorretos.";
+    }
+}
